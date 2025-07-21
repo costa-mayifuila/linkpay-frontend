9c0852e3375d49e63f5c508c861fdf45fc1cc484
Clóvis Costa <businessclovis@gmail.com>
2025-07-21 15:44:53 -0300

    Corrige roteamento SPA com vercel.json e vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,20 @@
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
-
-// https://vitejs.dev/config/
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
   server: {
-    port: 3000
+    port: 3000,
   },
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
   },
-  // 🔁 para suportar rotas no Vercel
-  base: '/'
+  base: '/',
 })
+ 
